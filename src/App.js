bcc55731f62d0120d4fbc22b030c461684201684
Donahi Casas-Silva <DCS@Donahis-MacBook-Pro.local>
2019-02-13 00:55:16 -0800

    Assignment 2 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBGZTNbC-ju1KtmC-WDrV0Q-LRB8-sMp88",
+    authDomain: "bloc-chat-76c18.firebaseapp.com",
+    databaseURL: "https://bloc-chat-76c18.firebaseio.com",
+    projectId: "bloc-chat-76c18",
+    storageBucket: "bloc-chat-76c18.appspot.com",
+    messagingSenderId: "268584792738"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+        <h1>Bloc Chat!</h1>
         </header>
+        <div>
+        <RoomList firebase={firebase}/>
+        </div>
       </div>
     );
   }
